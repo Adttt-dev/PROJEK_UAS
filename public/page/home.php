a27865cd8604f3ac191674ffb83f86e51d772c70
DitCoding <ditcoding@gmail.com>
2024-11-07 12:35:00 +0700

    first commit

---
+++ public/page/home.php
@@ -0,0 +1,213 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sampah</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="../src/css/main.css">
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light px-4 sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">SAMPAH <span>MASYARAKAT</span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PETA TITIK SAMPAH</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ASET</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PEMBUANGAN SAMPAH ILEGAL</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <!-- menu -->
+  <section class="main-bg mt-4">
+    <div class="container px-4">
+      <div class="row">
+        <div class="col-lg-6 text-center align-self-center">
+          <div class="">
+            <h1><span>Lorem, ipsum.</span> Lorem ipsum dolor sit amet.</h1>
+            <p">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor, magni!</p>
+              <a
+                href="page-html"
+                class="btn btn-warning">
+                COBA SEKARANG
+              </a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="px-4 py-5">
+            <img
+              src="../src/img/home.png"
+              class="px-4 py-5 w-100" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <!-- layanan -->
+    <section id="layanan" class="p-5 bg-light text-dark">
+    <div class="container pt-3 text-center">
+      <div class="container-fluid pt-5 text-center">
+        <h2 class="title-project">
+          MARI
+          <span>BERGERAK</span>
+        </h2>
+        <p class="subtitle-project">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, voluptatem.</p>
+        <div class="row py-4">
+          <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+              <img src="../src/img/layanan/aset.png" class="card-img-top" alt="my-porto">
+              <div class="card-body">
+                <h5 class="card-title">PETA</h5>
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro ipsa recusandae excepturi libero sed molestiae distinctio voluptates corporis mollitia atque.</p>
+                <a href="#" class="btn btn-warning">PETA</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+              <img src="../src/img/layanan/aset.png" class="card-img-top" alt="my-porto">
+              <div class="card-body">
+                <h5 class="card-title">ASET</h5>
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro ipsa recusandae excepturi libero sed molestiae distinctio voluptates corporis mollitia atque.</p>
+                <a href="#" class="btn btn-warning">ASET</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+              <img src="../src/img/layanan/aset.png" class="card-img-top" alt="my-porto">
+              <div class="card-body">
+                <h5 class="card-title">LAPORAN</h5>
+                <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro ipsa recusandae excepturi libero sed molestiae distinctio voluptates corporis mollitia atque.</p>
+                <a href="#" class="btn btn-warning">LAPORAN</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  </section>
+
+  
+  <!-- edu -->
+  <section
+    id="upgrade"
+    class="main-color">
+    <div class="container py-5 px-4">
+      <div class="row">
+        <div class="col-lg-6 px-2">
+          <p class="atas-judul">Lorem ipsum dolor sit.</p>
+          <h2>Lorem, ipsum dolor.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptate repellendus aut odio delectus quisquam dignissimos cum accusantium cumque obcaecati!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsum odio laboriosam, debitis quod, eaque repudiandae alias, dolore quas obcaecati minus? Totam nobis eveniet culpa at, quis illum. Iure, molestias!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ratione fugiat atque tenetur non nesciunt deleniti laborum aliquam delectus neque.</p>
+          <a
+            href="page-html"
+            class="btn btn-warning">
+            Lorem, ipsum.
+          </a>
+        </div>
+        <div class="col-lg-6 px-5 align-self-center">
+          <img
+            src="../src/img/peduliSampah.png"
+            class="w-100" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="sertifikasi">
+    <div class="container py-5 px-4">
+      <div class="row">
+        <div class="col-lg-6 px-5 align-self-center">
+          <img
+            src="../src/img/peduliSampah.png"
+            class="w-100" />
+        </div>
+        <div class="col-lg-6 px-2">
+          <p class="atas-judul">Lorem, ipsum dolor.</p>
+          <h2>Lorem ipsum dolor sit.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptate repellendus aut odio delectus quisquam dignissimos cum accusantium cumque obcaecati!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsum odio laboriosam, debitis quod, eaque repudiandae alias, dolore quas obcaecati minus? Totam nobis eveniet culpa at, quis illum. Iure, molestias!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ratione fugiat atque tenetur non nesciunt deleniti laborum aliquam delectus neque.</p>
+          <a
+            href="page-html"
+            class="btn btn-warning">
+            Lorem, ipsum dolor.
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer -->
+  <footer class="main-footer-menu bg-light main-nav py-5 ">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 mb-3">
+          <div class="mb-3">
+            <a href="home.php" class="main-logo-footer">SAMPAH <span>MASYARAKAT</span></a>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, provident?</p>
+          <p>Perduli dengan sampah &hearts;</p>
+        </div>
+        <div class="col-lg-3 mb-3">
+          <h3>Lorem, ipsum.</h3>
+          <ul>
+            <li><a href="">lorem</a></li>
+            <li><a href="">Lorem, ipsum.</a></li>
+            <li><a href="">lorem</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 mb-3">
+          <h3>Lorem, ipsum.</h3>
+          <ul>
+            <li><a href="">lorem</a></li>
+            <li><a href="">lorem</a></li>
+            <li><a href="">lorem</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 mb-3">
+          <h3>lorem</h3>
+          <ul>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem ipsum dolor sit.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div class="text-center py-3 main-footer-copy bg-light">pemrograman web dasar &copy; 2024 bebas sampah</div>
+  <!-- footer end -->
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
